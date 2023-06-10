ae7f5caa401f742fa69879445f2ee1f97ee9d7cb
Marek Linka <marek.linka@outlook.com>
2023-06-10 12:04:57 +0200

    Replace CSS with SCSS Clean up style resources

---
+++ backend/FourthPharos.Host/Configuration/ServiceConfiguratin.cs
@@ -0,0 +1,16 @@
+using FourthPharos.Host.Services;
+
+namespace FourthPharos.Host.Configuration;
+public static class ServiceConfiguration
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services)
+    {
+#if DEBUG
+        services.AddSassCompiler();
+#endif
+
+        services.AddTransient<ICircleService, CircleService>();
+
+        return services;
+    }
+}
